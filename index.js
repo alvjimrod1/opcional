2445f29fe94914a1b80016e75ed941e7e9bd3a25
sos171811brr <balramrom95@gmail.com>
2018-03-02 13:22:48 +0000

    sourse /hello working

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var express = require("express");
+var app = express();
 
+app.get("/hello", (req,res) =>{
+    res.send("Hello World")
+});
 
-console.log("Hello World!")
+app.listen(process.env.PORT);
