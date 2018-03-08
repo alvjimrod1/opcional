861ee4af4341e614038fabc66b17eae76082fd9a
sos171811brr <balramrom95@gmail.com>
2018-03-08 17:13:55 +0000

    public done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
+var port = (process.env.PORT||1607);
+
 
 app.get("/hello", (req,res) =>{
     res.send("Hello World")
 });
 
-app.listen(process.env.PORT);
+app.use("/",express.static("/home/ubuntu/workspace/SOS1718-09/public"));
+
+app.listen(port,()=>{
+    console.log("Server ready on port "+port+"!");
+}).on("error", (e)=>{
+    console.log("Server not ready "+ e);
+});
