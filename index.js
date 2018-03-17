c4fb8169191fe609ff7c48711af3e276396af702
sos171811brr <balramrom95@gmail.com>
2018-03-17 19:29:40 +0000

    span-univ-stats api

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require("express");
-var app = express();
+
 var port = (process.env.PORT||1607);
 
+
+var app = express();
+
+
+app.use("/",express.static(__dirname +"/public"));
 
 app.get("/hello", (req,res) =>{
     res.send("Hello World")
 });
 
-app.use("/",express.static(__dirname +"/public"));
+
 
 app.listen(port,()=>{
     console.log("Server ready on port "+port+"!");
