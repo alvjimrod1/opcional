aea6a622eed643b9b33bcf261dba0a92180f4051
sos171811brr <balramrom95@gmail.com>
2018-04-21 17:26:56 +0000

    fe

--- public/spanUnivStats/listCtrl.js
+++ public/spanUnivStats/listCtrl.js
@@ -6,45 +6,38 @@ angular.module("SpanUnivStatsManagerApp").controller("ListCtrl", ["$scope", "$ht
 
     $scope.addStat = function() {
         $http.post(api, $scope.newStat).then(function(response) {
-            console.log("response.status = "+response.status );
-            /*
-            if (response.status == 500) {
-                $scope.status = "Error accesing database. Sorry, try it later (" + response.status + ")";
+            console.log("response.status = " + response.status);
+
+            switch (response.status) {
+                case 500:
+                    $scope.status = "Error accesing database. Sorry, try it later (" + response.status + ")";
+                case 400:
+                    $scope.status = "It is necesary to fill in all the fields (" + response.status + ")";
+                case 409:
+                    $scope.status = "Conflict. There is already a stat for " + $scope.newStat.autCommunity + ", " + $scope.newStat.year + " (" + response.status + ")";
+                case 201:
+                    $scope.status = "ADD method status :  Correctly created (" + response.status + ")";
 
             }
-            else if (response.status == 400) {
-                $scope.status = "It is necesary to fill in all the fields (" + response.status + ")";
-
-            }
-            if (response.status == 409) {
-                $scope.status = "Conflict. There is already a stat for " + $scope.newStat.autCommunity + ", " + $scope.newStat.year +  " (" + response.status + ")";
-
-            }*/
-
-            if (response.status == 201) {
-                
-                $scope.status = "ADD method status :  Correctly created (" + response.status + ")";
-            }
-
             getSpanUnivStats();
         });
     }
-    
-    $scope.deleteStat = function(autCommunity,year) {
-        console.log("Stat to be deleted: Stat of " + autCommunity +" in "+ year);
-        $http.delete(api+"/"+autCommunity+"/"+year).then(function(response) {
+
+    $scope.deleteStat = function(autCommunity, year) {
+        console.log("Stat to be deleted: Stat of " + autCommunity + " in " + year);
+        $http.delete(api + "/" + autCommunity + "/" + year).then(function(response) {
             $scope.status = "DELETE method status :  Correctly deleted (" + response.status + ")";
             getSpanUnivStats();
         });
     }
-    
+
     $scope.deleteAllStats = function() {
         $http.delete(api).then(function(response) {
             $scope.status = "DELETE method status :  Correctly deleted (" + response.status + ")";
             getSpanUnivStats();
         });
     }
-    
+
 
     function getSpanUnivStats() {
         $http.get(api).then(function(response) {
