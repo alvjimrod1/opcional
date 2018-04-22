dc305ac750924ae3b5b0ea00d2f19c32a151fc32
sos171809ajr <alvarojimenezro@hotmail.es>
2018-04-22 00:19:47 +0000

    fallo arreglado de modulo

--- public/spanishUniversities/spanishUniversitiesManagerApp.js
+++ public/spanishUniversities/spanishUniversitiesManagerApp.js
@@ -1,14 +1,14 @@
 /* global angular */
 angular
- .module("SpanishUniversitiesManagerApp", ["ngRoute"])
- .config(function($routeProvider) {
-  $routeProvider
-   .when("/", {
-    templateUrl: "list.html",
-    controller: "ListCtrl"
-   }).when("/univ/:autCommunity/:yearFund", {
-    templateUrl: "edit.html",
-    controller: "EditCtrl"
-   });
+    .module("spanishUniversitiesManagerApp", ["ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "list.html",
+                controller: "ListCtrl"
+            }).when("/univ/:autCommunity/:yearFund", {
+                templateUrl: "edit.html",
+                controller: "EditCtrl"
+            });
 
- });
+    });
