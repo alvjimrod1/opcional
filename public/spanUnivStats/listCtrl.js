82e695270e0f027e8743559c31157010a1d2d9a5
sos171811brr <balramrom95@gmail.com>
2018-05-05 16:03:15 +0000

    ñodjnflv

--- public/spanUnivStats/listCtrl.js
+++ public/spanUnivStats/listCtrl.js
@@ -1,5 +1,6 @@
 /* global angular */
 /* global $ */
+/* global Highcharts */
 
 
 angular.module("SpanUnivStatsManagerApp").controller("ListCtrl", ["$scope", "$http", function($scope, $http) {
@@ -31,6 +32,7 @@ angular.module("SpanUnivStatsManagerApp").controller("ListCtrl", ["$scope", "$ht
             }
             delete $scope.newStat;
             getSpanUnivStats();
+            
         });
 
 
@@ -62,9 +64,11 @@ angular.module("SpanUnivStatsManagerApp").controller("ListCtrl", ["$scope", "$ht
             console.log($scope.stats.length);
         });
         search = "?";
+        getHighchart();
     };
 
     getSpanUnivStats();
+    getHighchart();
 
 
 
@@ -137,6 +141,105 @@ angular.module("SpanUnivStatsManagerApp").controller("ListCtrl", ["$scope", "$ht
         getSpanUnivStats();
         delete $scope.searchedStat;
     }
+    
+    
+    
+      /* CHARTS */
+
+    /* eliminar elementos duplicados*/
+
+    Array.prototype.unique = function(a) {
+        return function() { return this.filter(a) }
+    }(function(a, b, c) {
+        return c.indexOf(a, b + 1) < 0
+    });
+
+    /*ordenar array*/
+
+    Array.prototype.sortNumbers = function() {
+        return this.sort(
+            function(a, b) {
+                return a - b
+            }
+        );
+    }
+
+function getHighchart(){
+
+    var enr = [];
+    var years = [];
+
+    $http.get(api).then(function(response) {
+        console.log(response.data[0].enrolledNumber);
+        for (var i = 0; i < response.data.length; i++) {
+            years.push(response.data[i].year);
+
+        }
+        
+        var totalEnrolledNumber = [];
+
+        for (var i = 0; i < years.sortNumbers().unique().length; i++) {
+            var yearEnrolledNumber = 0;
+            for (var j = 0; j < response.data.length; j++) {
+                if (response.data[j].year == years.sortNumbers().unique()[i]) {
+                    yearEnrolledNumber += response.data[j].enrolledNumber;
+                }
+            }
+            totalEnrolledNumber.push(yearEnrolledNumber);
+
+        }
+
+        Highcharts.chart('highcharts', {
+
+            title: {
+                text: 'Spanish Universities Statistics'
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Enrolled Number'
+                }
+            },
+            xAxis: {
+                categories: years.sortNumbers().unique()
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: true
+                    }
+                }
+            },
+
+            series: [{
+                name: 'Enrolled',
+                data: totalEnrolledNumber
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+
+        });
+    });
+};
 
 
 }]);
