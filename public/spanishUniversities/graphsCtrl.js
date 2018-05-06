7ebacfd0c04dd0ee53ed7778f2fc01be110432c3
sos171809ajr <alvarojimenezro@hotmail.es>
2018-05-06 19:11:19 +0000

    fixes all

--- public/spanishUniversities/graphsCtrl.js
+++ public/spanishUniversities/graphsCtrl.js
@@ -16,13 +16,14 @@ angular.module("spanishUniversitiesManagerApp").controller("graphsCtrl", ["$scop
         return c.indexOf(a, b + 1) < 0;
     });
 
-
+    var auxiliarArray = [];
     var autCommunities = [];
-  //  var hq = [];
+    //  var hq = [];
     var googleChartData = [
         ["Aut Community", "Number of universities"]
     ];
 
+    var resChartjs = [];
 
 
 
@@ -30,7 +31,7 @@ angular.module("spanishUniversitiesManagerApp").controller("graphsCtrl", ["$scop
         // console.log(response.data);
         for (var i = 0; i < response.data.length; i++) {
             autCommunities.push(response.data[i].autCommunity);
-         //   hq.push(response.data[i].headquar);
+            //   hq.push(response.data[i].headquar);
 
         }
         console.log("AUTCOMUNITIES" + autCommunities)
@@ -56,6 +57,9 @@ angular.module("spanishUniversitiesManagerApp").controller("graphsCtrl", ["$scop
 
             totalPublicas.push(acumPublicas);
             totalPrivadas.push(acumPrivadas);
+            console.log("acumPublicas" + acumPublicas);
+            console.log("acumPrivadas" + acumPrivadas)
+
 
         }
 
@@ -76,15 +80,19 @@ angular.module("spanishUniversitiesManagerApp").controller("graphsCtrl", ["$scop
 
         for (var i = 0; i < response.data.length; i++) {
             var cont = 0;
+
             for (var j = 0; j < response.data.length; j++) {
                 if (response.data[j].autCommunity == response.data[i].autCommunity) {
                     cont++;
                 }
             }
-            googleChartData.push([response.data[i].autCommunity, cont])
+
+            googleChartData.push([response.data[i].autCommunity, cont]);
+
+
+
         }
 
-        // console.log(cont)
 
 
         Highcharts.chart('container', {
@@ -120,40 +128,116 @@ angular.module("spanishUniversitiesManagerApp").controller("graphsCtrl", ["$scop
                 data: totalPrivadas //sacar las universidades privadas 
             }, ]
         });
+        /*GOOGLE CHARTS*/
+
+        google.charts.load('current', {
+            'packages': ['geochart'],
+            // Note: you will need to get a mapsApiKey for your project.
+            // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+            'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+        });
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+
+        function drawRegionsMap() {
+            var data = google.visualization.arrayToDataTable(googleChartData);
+
+            var options = {
+                region: 'ES',
+                resolution: 'provinces',
+
+
+                colorAxis: {
+                    minValue: 0,
+                    maxValue: 10
+                }
+            };
+
+
+            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+            chart.draw(data, options);
+        }
+        /*ANGULAR CHART.JS*/
+
+        for (var i = 0; i < autCommunities.unique().length; i++) {
+            var acum = 0;
+            for (var j = 0; j < response.data.length; j++) {
+                if ((response.data[j].autCommunity) == autCommunities.sort().unique()[i]) {
+                    acum += 1;
+                }
+            }
+            resChartjs.push(acum);
+
+        }
+
+
+        // console.log(totalPrivadas)
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: autCommunities.unique(),
+                datasets: [{
+                    label: 'Number of universities',
+                    data: resChartjs,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
 
 
 
     })
 
-    /*GOOGLE CHARTS*/
-
-    google.charts.load('current', {
-        'packages': ['geochart'],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-    });
-    google.charts.setOnLoadCallback(drawRegionsMap);
-
-
-    function drawRegionsMap() {
-        var data = google.visualization.arrayToDataTable(googleChartData);
-
-        var options = {
-            region: 'ES',
-            resolution: 'provinces',
-
-
-            colorAxis: {
-                minValue: 0,
-                maxValue: 10
-            }
-        };
-
-
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
-        chart.draw(data, options);
-    }
 
 }]);
