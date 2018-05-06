c3d5a4728aae09d05c2b3193e34c0a844c817ba2
sos171811brr <balramrom95@gmail.com>
2018-05-06 12:31:24 +0000

    testMoved

---
+++ test/spanUnivStats/config.js
@@ -0,0 +1,10 @@
+exports.config = {
+
+    seleniumAddress: 'http://localhost:8910', //dirección del navegador al que (protractor) le va a lanzar las pruebas (phantomjs)
+
+    specs: ['T01-loadData.js', 'T02-createStat.js'], // array con los test a lanzar
+
+    capabilities: { // tipo de navegador que voy a usar
+        'browserName': 'phantomjs'
+    }
+};
