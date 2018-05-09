bce82873a3de4cad37424642078d5e3cf58da3ee
sos171809ajr <alvarojimenezro@hotmail.es>
2018-05-09 14:13:59 +0000

    fali

--- public/AppManager.js
+++ public/AppManager.js
@@ -42,13 +42,13 @@ angular
 
                 /*RAFA*/
             }).when("/openSourceContests", {
-                templateUrl: "../openSourceContests/list/list.html",
+                templateUrl: "/openSourceContests/list/list.html",
                 controller: "ListCtrl"
             }).when("/openSourceContests/contest/:year/:university/:project", {
-                templateUrl: "../openSourceContests/edit/edit.html",
+                templateUrl: "/openSourceContests/edit/edit.html",
                 controller: "EditCtrl"
             }).when("/openSourceContests/graph", {
-                templateUrl: "../openSourceContests/graph/graph.html",
+                templateUrl: "/openSourceContests/graph/graph.html",
                 controller: "GraphCtrl"
             });
     });
